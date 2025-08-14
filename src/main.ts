40718a717c6f8bca74da9b645333b6bdac625f6f
ThanhXT2002 <tranxuanthanhtxt2002@gmail.com>
2025-08-14 23:33:25 +0700

    feat: add Swagger integration for API documentation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = process.env.PORT || 3000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Insurance API')
+    .setDescription('API documentation for the Insurance project')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(port);
 }
 bootstrap();
